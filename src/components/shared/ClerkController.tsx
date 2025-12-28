9e59c258afcfa1a855107d96b4c2a49fe57de05c
unknown <khaledsameh358@gmail.com>
2025-12-28 03:13:16 +0200

    add flexbelity between orders button and user avatar in header

--- src/components/shared/ClerkController.tsx
+++ src/components/shared/ClerkController.tsx
@@ -27,6 +27,7 @@ const ClerkController = () => {
             initial={{ opacity: 0, y: -10 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.3 }}
+            className="flex items-center gap-4"
           >
             <Button
               variant={"link"}
